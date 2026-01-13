d0f98d06e945c517b65599bfce196817cc0a7292
unknown <alauddinnajim202@gmail.com>
2026-01-13 15:48:45 +0600

    manage working hours

--- app/Models/ProfessinalWorkingHour.php
+++ app/Models/ProfessinalWorkingHour.php
@@ -1,10 +1,26 @@
 <?php
-
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class ProfessinalWorkingHour extends Model
 {
     protected $fillable = ['day', 'is_closed', 'open_time', 'close_time'];
+
+    // WorkingHour model
+    public function getOpenTime12Attribute()
+    {
+        return $this->open_time
+            ? Carbon::createFromFormat('H:i', $this->open_time)->format('h:i A')
+            : null;
+    }
+
+    public function getCloseTime12Attribute()
+    {
+        return $this->close_time
+            ? Carbon::createFromFormat('H:i', $this->close_time)->format('h:i A')
+            : null;
+    }
+
 }
